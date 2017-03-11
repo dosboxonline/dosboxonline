902c50f64a0a06a526b9544b1b77f5f87b5826b1
DOSBoxOnline <anton.anatolevich@gmail.com>
2017-03-11 09:49:01 +0300

    Update loader.js

--- games90s/atari2600/loader.js
+++ games90s/atari2600/loader.js
@@ -53,17 +53,17 @@ var Module = null;
      // yea, this is a hack
      if (/archive\.org$/.test(document.location.hostname)) {
        var images = { ia: img("atari-2600.png"),
-                      mame: img("pixel.png"),
-                      mess: img("pixel.png"),
-                      dosbox: img("pixel.png"),
-                      sae: img("pixel.png")
+                      mame: img("mess-logo.png"),
+                      mess: img("mess-logo.png"),
+                      dosbox: img("mess-logo.png"),
+                      sae: img("mess-logo.png")
                     };
      } else {
        images = { ia: img("atari-2600.png"),
-                  mame: img("pixel.png"),
-                  mess: img("pixel.png"),
-                  dosbox: img("pixel.png"),
-                  sae: img("pixel.png")
+                  mame: img("mess-logo.png"),
+                  mess: img("mess-logo.png"),
+                  dosbox: img("mess-logo.png"),
+                  sae: img("mess-logo.png")
                 };
      }
 
