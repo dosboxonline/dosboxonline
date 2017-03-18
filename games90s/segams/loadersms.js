a798dcf4d96e27cdde72b959edee446304c1d5ce
DOSBoxOnline <anton.anatolevich@gmail.com>
2017-03-18 21:06:02 +0300

    Update loadersms.js

--- games90s/segams/loadersms.js
+++ games90s/segams/loadersms.js
@@ -1122,8 +1122,8 @@ var Module = null;
                               statusCell.textContent = "✔";
                               titleCell.textContent = title;
                               titleCell.style.fontWeight = 'bold';
-                              titleCell.parentNode.style.backgroundColor = splash.getColor('#fafafa');
-                              titleCell.parentNode.style.color = splash.getColor('#333333');
+                              titleCell.parentNode.style.backgroundColor = splash.getColor('foreground');
+                              titleCell.parentNode.style.color = splash.getColor('background');
                             }
                             function failure() {
                               statusCell.textContent = "✘";
